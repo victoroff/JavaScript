b0780027731643abe30f2256b648b4a929e55315
Bebbo <7547244+victoroff@users.noreply.github.com>
2020-01-22 17:45:54 +0200

    task class

--- todo-list/todo.js
+++ todo-list/todo.js
@@ -1,5 +1,6 @@
-class Todo{
-    constructor(task){
-        this.task = task
-    }
-}
+// import Task require(@'task.js')
+// class Todo{
+//     constructor(tasks){
+//         this.tasks = new List<Task>();
+//     }
+// }
